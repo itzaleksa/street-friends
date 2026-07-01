0a219039884bbd0473ba53b66441420368a56e57
Mystery24601 <arthur-de-jong@hotmail.com>
2026-07-01 17:40:25 +0200

    Fix for First Commit: - Fixed middle-container alignment issue - Added Favicon - Navigation Container is dynamically generated now - About Title is capitalized - Added Invisible Separator - Added cash.min.js to js folder (instead of a CDN. this is to ensure the site stays working) - Fixed Alt text for social media images to represent their social medias.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
-// const navigation_bar = `
+const navigation_bar = `
+<div class="title-container">
+    <div class="logo-container">
+        <img class="logo" src="assets/images/SFT_Logo.png" alt="Street Friends Logo">
+    </div>
+    <div class="separator"></div>
+    <nav class="nav-container">
+        <a id="credits-button" class="nav-button" href="html/sft_credits.html">
+            <img class="nav-icon" src="assets/images/credits.png" alt="Credits">
+        </a>
+        <a onclick="" id="about-button" class="nav-button" href="#">
+            <img class="nav-icon" src="assets/images/about.png" alt="About">
+        </a>
+        <a id="shows-button" class="nav-button" href="html/sft_shows.html">
+            <img class="nav-icon" src="assets/images/shows.png" alt="Shows">
+        </a>
+    </nav>
+</div>
+`;
 
-// `;
-
-// $(".middle-container").prepend(navigation_bar)
+$(function() {
+    $(".middle-container").prepend(navigation_bar)
+    console.log("Added navigation_bar!")
+});
