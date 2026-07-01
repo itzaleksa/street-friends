45f9345e94715cffaf9b2c70069022d7c39bb965
Mystery24601 <arthur-de-jong@hotmail.com>
2026-07-01 20:33:39 +0200

    Dynamic Generation Update: - The header and footer parts (Navigation Bar & Support us Container) are dynamically generated - Changed sft.css to about.css - Changed the README to include a license section and to fix the markdown

--- README.txt
+++ README.txt
@@ -1,3 +1,12 @@
 # About
+
 This repository contains the website for Street Friends, hosted using GitHub Pages.
 It used to be a linktree site, but due to ai slop data collection, we switched over to GitHub.
+
+
+# License
+
+Copyright © 2026 Street Friends Team. All rights reserved.
+No part of this repository (including code, design, assets, and branding) may be reproduced, distributed, or transmitted in any form or by any means, including cloning this repository, without the prior written permission of the author.
+
+(Any fanart is excluded from this license)
--- js/main.js
+++ js/main.js
@@ -1,24 +1,7 @@
-const navigation_bar = `
-<div class="title-container">
-    <div class="logo-container">
-        <img class="logo" src="assets/images/SFT_Logo.png" alt="Street Friends Logo">
-    </div>
-    <div class="separator"></div>
-    <nav class="nav-container">
-        <a id="credits-button" class="nav-button" href="html/sft_credits.html">
-            <img class="nav-icon" src="assets/images/credits.png" alt="Credits">
-        </a>
-        <a onclick="" id="about-button" class="nav-button" href="#">
-            <img class="nav-icon" src="assets/images/about.png" alt="About">
-        </a>
-        <a id="shows-button" class="nav-button" href="html/sft_shows.html">
-            <img class="nav-icon" src="assets/images/shows.png" alt="Shows">
-        </a>
-    </nav>
-</div>
-`;
+const IS_LOCAL = window.location.protocol === 'file:';
 
-$(function() {
-    $(".middle-container").prepend(navigation_bar)
-    console.log("Added navigation_bar!")
-});
+const PATHS = {
+    assets: IS_LOCAL ? '../assets/' : '/my-project/assets/',
+    js:     IS_LOCAL ? '../js/'     : '/my-project/js/',
+    img:    IS_LOCAL ? '../assets/images/' : '/my-project/assets/images/'
+};
