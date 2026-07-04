0aa4b1b3b23ab0ffdbd531a5b108ae8abeaa36fd
Mystery24601 <arthur-de-jong@hotmail.com>
2026-07-04 18:33:10 +0200

    First Street Friends Show About Page Update: - Restructured some CSS to work with the picture images. (and to fix an annoying overflow issue) - Changed about-title and about-description id's to be more generic names (and added that as classes to the main.css file) -  Added character pictures to the Street Friends Show about page (+ a description and titles above it) - Made the background image based on the page's css file rather than within a global css file. - Removed the dashed border for the middle-container (It wasn't needed) - More separator classes (and changed the old separator classes to be smaller) - Navigation buttons now use vw (viewport width) measurements instead of vh (viewport height) measurements. - Lots of additions made to sf_about.css - Changed main_elements_sf.js navigation bar code to navigate to the street friends show pages - Added a chibi_bar constant that takes place of the support-container (sort of, it uses the support container still)
    And probably lots more that I forgot to say!

--- js/main_elements_sf.js
+++ js/main_elements_sf.js
@@ -1,23 +1,40 @@
 const navigation_bar = `
 <div class="title-container">
     <div class="logo-container">
-        <img class="logo" src="assets/images/navigation/SFT_Logo.png" alt="Street Friends Logo">
+        <img class="logo" src="assets/images/navigation/SF_Logo.png" alt="Street Friends Logo">
     </div>
     <div class="separator"></div>
     <nav class="nav-container">
-        <a class="nav-button" href="sft_credits.html">
-            <img class="nav-icon" src="assets/images/navigation/credits.png" alt="Episodes">
+        <a class="nav-button" href="sf_episodes.html">
+            <img class="nav-icon" src="assets/images/navigation/episodes.png" alt="Episodes">
         </a>
-        <a class="nav-button" href="index.html">
+        <a class="nav-button" href="sf_about.html">
             <img class="nav-icon" src="assets/images/navigation/about.png" alt="About">
         </a>
-        <a class="nav-button" href="sft_projects.html">
-            <img class="nav-icon" src="assets/images/navigation/projects.png" alt="Explore">
+        <a class="nav-button" href="sf_explore.html">
+            <img class="nav-icon" src="assets/images/navigation/explore.png" alt="Explore">
         </a>
     </nav>
 </div>
 `;
 
+const chibi_bar = `
+<div class="support-container">
+    <div class="chibi-section">
+        <img class="chibi" src="assets/images/splashes/doggie_chibi_splash.png" alt="Doggie Chibi">
+        <img class="chibi" src="assets/images/splashes/alek_chibi_splash.png" alt="Alek Chibi">
+    </div>
+    <a id="home-button" href="index.html">
+        <img id="home-image" class="nav-icon" src="assets/images/navigation/home-button.png" alt="Home">
+    </a>
+    <div class="chibi-section">
+        <img class="chibi" src="assets/images/splashes/qd_chibi_splash.png" alt="QD Chibi">
+        <img class="chibi" src="assets/images/splashes/squiddo_chibi_splash.png" alt="Squiddo Chibi">
+    </div>
+</div>
+`;
+
 $(function() {
     $(".middle-container").prepend(navigation_bar);
+    $(".middle-container").append(chibi_bar);
 });
